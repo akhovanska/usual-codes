7c9f5eee067c8630dc78aa7fba565d4c13fcf07d
Anastasiia Khovanska <98605728+akhovanska@users.noreply.github.com>
2022-09-19 13:24:37 +0300

    Update singleton2.py

--- singleton2.py
+++ singleton2.py
@@ -1,18 +1,17 @@
 class Singleton:
-    __instance = None
 
+    __instance = None
     def __init__(self):
         if not Singleton.__instance:
-            print(" __init__ method called. .")
+            print(" __init__ method called..")
         else:
             print("Instance already created:", self.getInstance())
-
     @classmethod
     def getInstance(cls):
         if not cls.__instance:
             cls.__instance = Singleton()
         return cls.__instance
+s = Singleton() ## клас ініціалізований, але об’єкт не створений
+print("Object created", Singleton.getInstance()) # Тут створюється об’єкт
+s1 = Singleton() ## вже створений екземпляр
 
-
-s = Singleton()
-print("object created", Singleton.getInstance())
